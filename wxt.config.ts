8e4e29138f9668e901073feabc4d5d1b49fc11fe
Fahad Iftikhar <fahad090np@gmail.com>
2026-07-11 12:18:29 +0500

    Configure Manifest Permissions And Host Permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'Webshot',
+    description:
+      'Capture full page, selected sections, or viewport in PNG, JPG, WEBP, SVG, or PDF with custom resolution',
+    permissions: ['activeTab', 'downloads', 'scripting'],
+    host_permissions: ['<all_urls>'],
+    action: {
+      default_title: 'Capture page with Webshot',
+    },
+  },
 });
